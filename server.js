aa8c9bd16ebb8152525bbd1ffcaaafbe1a0abcea
ife popoola <ifesco20072009@gmail.com>
2024-10-23 11:12:36 +0100

    align profile and orders

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json());
 // Use the user routes
 app.use('/api', userRoutes); // Prefix the user routes with /api
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not Found' });
+  });
+  
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
